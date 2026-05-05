625fc1d01f0f68ffeaa0667d8e24602166765449
Dthx-956 <2143875512@qq.com>
2026-05-05 09:24:51 +0800

    Add drop_table method to DefaultHandler

--- src/observer/storage/default/default_handler.cpp
+++ src/observer/storage/default/default_handler.cpp
@@ -100,7 +100,14 @@ RC DefaultHandler::create_db(const char *dbname)
   return RC::SUCCESS;
 }
 
-RC DefaultHandler::drop_db(const char *dbname) { return RC::INTERNAL; }
+RC DefaultHandler::drop_table(const char *dbname, const char *relation_name) 
+{
+    Db *db = find_db(dbname);
+    if (db == nullptr) {
+        return RC::SCHEMA_DB_NOT_OPENED;
+    }
+    return db->drop_table(relation_name);
+}
 
 RC DefaultHandler::open_db(const char *dbname)
 {
