73104af128c78dab8dfe758513217a7d9af114a3
Dthx-956 <2143875512@qq.com>
2026-05-05 11:47:32 +0800

    Include boolean_type.h in date_type.cpp
    Added include for boolean_type.h to date_type.cpp

--- src/observer/common/type/date_type.cpp
+++ src/observer/common/type/date_type.cpp
@@ -4,6 +4,7 @@
 #include "common/lang/sstream.h"
 #include "common/lang/comparator.h"
 #include "storage/common/column.h"
+#include "common/type/boolean_type.h"
 
 static bool check_date(int y, int m, int d)
 {
